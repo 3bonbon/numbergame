521e3563b49e31352ab60cdb47523802f3088b63
3bonbon <sambong.dev@gmail.com>
2018-03-17 00:14:15 -0600

    Add function to test for valid numbers

--- numbergame/model.py
+++ numbergame/model.py
@@ -1,10 +1,24 @@
+import string
 from collections import namedtuple
 
 Score = namedtuple('Score', 'maru sankaku')
 
 
+def is_valid_number(number):
+    if len(number) != 4:
+        return False
+    if len(set(number)) < 4:
+        return False
+    for digit in number:
+        if digit not in string.digits:
+            return False
+    return True
+
+
 class Player:
     def __init__(self, number):
+        if not is_valid_number(number):
+            raise ValueError(f'{number} is not a valid number')
         self._number = number
 
     def calc_score(self, guess):
@@ -16,3 +30,4 @@ class Player:
             elif gss in self._number:
                 sankaku += 1
         return Score(maru=maru, sankaku=sankaku)
+
